ad515dde312696f3ccac57d4051b3c943f3274f0
Alessio De Vito <93343041+alls-cpp@users.noreply.github.com>
2023-12-30 02:10:58 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -24,13 +24,21 @@ chown -R $username:$username /home/$username
 
 # Installing Programs 
 nala install i3 xorg kitty unzip wget pulseaudio pavucontrol build-essential curl nemo \
-chromium neofetch flameshot lightdm bluetoothctl telegram-desktop code fd-find fzf \
-zsh eog evince vlc suckless-tools bat -y
+chromium neofetch flameshot lightdm telegram-desktop fd-find fzf fonts-jetbrains-mono fonts-font-awesome \
+lxappearance papirus-icon-theme zsh eog evince vlc suckless-tools bat xclip brightnessctl playerctl i3blocks -y
 
 # Enable graphical login and change target from CLI to GUI
 systemctl enable lightdm
 systemctl set-default graphical.target
 
-chsh -s $(which zsh)
 
-# Install eza and move fzf into .config
+# Set zsh default shell
+
+# Install eza
+
+# Enable default user in lightdm
+# In /etc/lightdm/lightdm.conf
+# Go down the file until you get to:
+# # greeter-hide-users=false
+# Un-comment it, make sure the value is false.
+
