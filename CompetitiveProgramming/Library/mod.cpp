5c8a1cfad3a643b9a515a7a60d926057b66c3347
Alessio De Vito <93343041+alls-cpp@users.noreply.github.com>
2024-01-09 12:02:49 +0100

    Add files via upload

---
+++ CompetitiveProgramming/Library/mod.cpp
@@ -0,0 +1,84 @@
+/**
+ * Description: Modular arithmetic operations
+ * Source: https://codeforces.com/contest/1916/submission/239663995
+ */
+
+template <int MOD> struct mint {
+	static const int mod = MOD;
+	int v;
+	explicit operator int() const {
+		return v;
+	}  // explicit -> don't silently convert to int
+	mint() : v(0) {}
+	mint(long long _v) {
+		v = int((-MOD < _v && _v < MOD) ? _v : _v % MOD);
+		if (v < 0) v += MOD;
+	}
+	bool operator==(const mint &o) const { return v == o.v; }
+	friend bool operator!=(const mint &a, const mint &b) { return !(a == b); }
+	friend bool operator<(const mint &a, const mint &b) { return a.v < b.v; }
+	friend std::istream &operator>>(std::istream &is, mint &a) {
+		long long x;
+		is >> x;
+		a = mint(x);
+		return is;
+	}
+	friend std::ostream &operator<<(std::ostream &os, mint a) {
+		os << int(a);
+		return os;
+	}
+ 
+	mint &operator+=(const mint &o) {
+		if ((v += o.v) >= MOD) v -= MOD;
+		return *this;
+	}
+	mint &operator-=(const mint &o) {
+		if ((v -= o.v) < 0) v += MOD;
+		return *this;
+	}
+	mint &operator*=(const mint &o) {
+		v = int((long long)v * o.v % MOD);
+		return *this;
+	}
+	mint &operator/=(const mint &o) { return (*this) *= inv(o); }
+	friend mint pow(mint a, long long p) {
+		mint ans = 1;
+		assert(p >= 0);
+		for (; p; p /= 2, a *= a)
+			if (p & 1) ans *= a;
+		return ans;
+	}
+	friend mint inv(const mint &a) {
+		assert(a.v != 0);
+		return pow(a, MOD - 2);
+	}
+ 
+	mint operator-() const { return mint(-v); }
+	mint &operator++() { return *this += 1; }
+	mint &operator--() { return *this -= 1; }
+	friend mint operator+(mint a, const mint &b) { return a += b; }
+	friend mint operator-(mint a, const mint &b) { return a -= b; }
+	friend mint operator*(mint a, const mint &b) { return a *= b; }
+	friend mint operator/(mint a, const mint &b) { return a /= b; }
+};
+// const int MOD = 998244353;
+const int MOD = int(1e9) + 7;
+using MINT = mint<MOD>;
+vector<MINT> fact(1, 1);
+vector<MINT> inv_fact(1, 1);
+MINT n_choose_k(int n, int k) {
+    if (k < 0 || k > n) {
+        return 0;
+    }
+    while ((int) fact.size() < n + 1) {
+        fact.push_back(fact.back() * (int) fact.size());
+        inv_fact.push_back(1 / fact.back());
+    }
+    return fact[n] * inv_fact[k] * inv_fact[n - k];
+}
+void compute_fact_inv_fact(int n) {
+    while ((int) fact.size() < n + 1) {
+        fact.push_back(fact.back() * (int) fact.size());
+        inv_fact.push_back(1 / fact.back());
+    }
+}
