dd472aee122067ff8f6b107053f4ea8c8ce93e96
Alessio De Vito <93343041+alls-cpp@users.noreply.github.com>
2024-02-23 15:28:40 +0100

    Update colortheme.lua

--- dotconfig/nvim/lua/plugins/colortheme.lua
+++ dotconfig/nvim/lua/plugins/colortheme.lua
@@ -8,11 +8,10 @@ return {
   --   end
   -- }
   {
-    'maxmx03/solarized.nvim',
+    'shaunsingh/solarized.nvim',
     lazy = false,
     priority = 1000,
     config = function()
-      vim.o.background = 'light' -- or 'dark'
       vim.cmd.colorscheme 'solarized'
     end,
   }
