96a1473cf577436fcfa085e3065a6ddfca1e88ab
Alessio De Vito <alessiodevito73@gmail.com>
2024-03-26 10:47:50 +0100

    a

--- update.sh
+++ update.sh
@@ -2,6 +2,7 @@ cp -r ~/.config/nvim/ dotconfig
 cp -r ~/.config/i3/ dotconfig
 cp -r ~/.config/zsh/ dotconfig
 cp -r ~/.config/alacritty/ dotconfig
+cp -r ~/.config/clangd/ dotconfig
 git add *
 git commit -m 'a'
 git push
